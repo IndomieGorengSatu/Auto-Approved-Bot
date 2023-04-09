581c79198b4fe90860dabc94313c13b3c9664a64
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2023-04-10 04:59:55 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-app = Client(bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
+app = Client("bot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
 
 TEXT = "Hello {}, Welcome To {}"
 APPROVED = True  # set to True by default
