ec737a65593edfa64d6f98f58f482ca1a83fcafc
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2023-04-10 17:12:59 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,12 @@ CHAT_ID = [int(pr0fess0r_99) for pr0fess0r_99 in environ.get("CHAT_ID", None).sp
 TEXT = environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved")
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
+
+def send_welcome_message(client: Client, user_id: int):
+    text = "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved"
+    client.send_message(chat_id=user_id, text=text)
+
+
 @pr0fess0r_99.on_message(filters.private & filters.command(["start"]))
 async def start(client: pr0fess0r_99, message: Message):
     approvedbot = await client.get_me() 
@@ -20,14 +26,14 @@ async def start(client: pr0fess0r_99, message: Message):
               [ InlineKeyboardButton("➕️ Add Me To Your Chat ➕️", url=f"http://t.me/{approvedbot.username}?startgroup=botstart") ]]
     await client.send_message(chat_id=message.chat.id, text=f"**__Hello {message.from_user.mention} Iam Auto Approver Join Request Bot Just [Add Me To Your Group Channnl](http://t.me/{approvedbot.username}?startgroup=botstart) || Repo https://github.com/PR0FESS0R-99/Auto-Approved-Bot||**__", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
 
-@pr0fess0r_99.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
+@pr0fess0r_99.on_chat_join_request((filters.group | filters.channel) & filters.private & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
 async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
     chat=message.chat # Chat
     user=message.from_user # User
     print(f"{user.first_name} Joined 🤝") # Logs
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
-        await client.send_message(chat_id=user.id, text=TEXT.format(mention=user.mention, title=chat.title))
+        await send_welcome_message(client, chat_id=user.id, text=TEXT.format(mention=user.mention, title=chat.title))
     #   print("Welcome....")
 
 print("Auto Approved Bot")
